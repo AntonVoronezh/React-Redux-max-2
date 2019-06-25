f7872d23cc146d8c5d432ad741c55b97ba5c9313
AntonVoronezh <pstut@mail.ru>
2019-06-25 16:31:49 +0300

    add FETCH_PROFILE_FAILURE

--- src/store/actions/profile.js
+++ src/store/actions/profile.js
@@ -6,3 +6,22 @@ const addProfileIdAC = id => ({
 });
 
 export { addProfileIdAC, ADD_PROFILE_ID };
+
+const FETCH_PROFILE_REQUEST = 'FETCH_PROFILE_REQUEST';
+const fetchProfileequestAC = () => ({
+	type: FETCH_PROFILE_REQUEST,
+});
+
+const FETCH_PROFILE_SUCCESS = 'FETCH_PROFILE_SUCCESS';
+const fetchProfileSuccessAC = profile => ({
+	type: FETCH_PROFILE_SUCCESS,
+	profile,
+});
+
+const FETCH_PROFILE_FAILURE = 'FETCH_PROFILE_FAILURE';
+const fetchProfileFailureAC = errorMsg => ({
+	type: FETCH_PROFILE_FAILURE,
+	errorMsg,
+});
+
+export { FETCH_PROFILE_REQUEST, FETCH_PROFILE_SUCCESS, FETCH_PROFILE_FAILURE };
